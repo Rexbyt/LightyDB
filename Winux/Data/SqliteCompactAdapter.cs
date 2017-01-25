64deae2965680d3131aaf8df2cdcc893f0a03d92
Admin <Admin>
2017-01-25 16:29:37 +0300

    Create table in database

---
+++ Winux/Data/SqliteCompactAdapter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Xml;
+using Winux.Data;
+using System.Windows.Forms;
+
+namespace Winux.Data
+{
+	class SqliteCompactAdapter
+    {
+		public SqliteCompactAdapter(string connectionString) {
+			if (!Environment.OSVersion.ToString().Contains("Unix"))
+			{
+				WinSqliteCompact scp = new WinSqliteCompact(connectionString);
+			}
+		}
+    }
+}
